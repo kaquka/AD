c5c24ebae07cdbe7165c83f387630fd6cbda5b60
kaquka <avalos_luisangel@hotmail.com>
2023-10-18 13:47:31 -0600

    Datos particionados

--- leerDB.py
+++ leerDB.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sys
+import random
 
 with open(F"database.txt", "w") as archivo:
     sys.stdout = archivo
@@ -13,5 +14,16 @@ with open(F"database.txt", "w") as archivo:
     for column in datos.columns :
         moda = datos[column].value_counts()
         print(moda)
+        
+    random.seed(42)  # Esto establece una semilla para que los números aleatorios sean reproducibles.
+    indices_seleccionados = random.sample(range(len(datos)), 58)
+    entrenamiento = datos.loc[indices_seleccionados]
+
+    # Los elementos restantes se guardarán en otro DataFrame llamado 'prueba'.
+    prueba = datos.drop(indices_seleccionados)
+
+    # Finalmente, puedes guardar estos DataFrames en archivos CSV.
+    entrenamiento.to_csv('entrenamiento.csv', index=False)
+    prueba.to_csv('prueba.csv', index=False)   
 
     
